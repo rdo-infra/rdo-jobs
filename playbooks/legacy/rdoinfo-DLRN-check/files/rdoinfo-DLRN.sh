9d5509fbf96c66136d6aed44d7734d006c7d1cd1
Javier Pena <jpena@redhat.com>
2019-03-26 14:45:29 +0100

    Switch default rdoinfo tag to train-uc
    Since [1], we are switching the master tag to be train-uc, instead
    of stein-uc.
    
    [1] - https://review.rdoproject.org/r/19744
    
    Depends-On: https://review.rdoproject.org/r/19744
    Change-Id: I1a5b1db4d2bb442e7e3cda98adbf53b0e08e8be8

--- playbooks/legacy/rdoinfo-DLRN-check/files/rdoinfo-DLRN.sh
+++ playbooks/legacy/rdoinfo-DLRN-check/files/rdoinfo-DLRN.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Simple script to test several DLRN packages
-TAG="${1:-stein-uc}"
+TAG="${1:-train-uc}"
 PACKAGES_TO_BUILD="${2:-python-glanceclient}"
 PROJECT_DISTRO_BRANCH="rpm-master"
 
@@ -15,10 +15,10 @@ target="centos"
 baseurl="http://trunk.rdoproject.org/centos7/"
 src="master"
 branch=""
-tag="stein-uc"
+tag="train-uc"
 
 # If we are testing a commit on a specific branch, make sure we are using it
-if [[ "${TAG}" != "stein-uc" ]]; then
+if [[ "${TAG}" != "train-uc" ]]; then
     branch=$(echo $TAG | awk -F- '{print $1}')
     tag=$TAG
     baseurl="http://trunk.rdoproject.org/${branch}/centos7/"
