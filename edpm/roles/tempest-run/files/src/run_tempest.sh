9f545d7d3cf87db2d0494ba538f3c74b1ff09ae6
Arx Cruz <arxcruz@redhat.com>
2023-02-27 11:09:31 +0100

    DNM - Testing tempest on edpm
    Change-Id: I451a191fadc5254ee3a68ad610bf6fa78f69f24c

---
+++ edpm/roles/tempest-run/files/src/run_tempest.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+TEMPEST_DIR=/root/tempest/openshift
+
+pushd /root/tempest
+
+export OS_CLOUD=default
+
+tempest init openshift
+
+pushd $TEMPEST_DIR
+
+discover-tempest-config --os-cloud $OS_CLOUD --debug --create
+
+TEMPEST_LIST="/root/tempest/"
+if [ ! -z ${USE_EXTERNAL_FILES} ]; then
+    TEMPEST_LIST="/root/external_files/"
+fi
+
+tempest run \
+  --include-list ${TEMPEST_LIST}include.txt \
+  --exclude-list ${TEMPEST_LIST}exclude.txt
+
+RETURN_VALUE=$?
+
+echo "Generate subunit"
+stestr last --subunit > ${TEMPEST_LIST}testrepository.subunit
+
+echo "Generate html result"
+subunit2html ${TEMPEST_LIST}testrepository.subunit ${TEMPEST_LIST}/stestr_results.html
+
+exit ${RETURN_VALUE}
+
+popd
+popd
