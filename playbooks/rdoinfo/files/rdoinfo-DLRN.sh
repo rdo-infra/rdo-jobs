9e372bd9ca9684c3ef3dbc39f8ac7c440874ef79
Alfredo Moralejo <amoralej@redhat.com>
2024-03-07 12:56:58 +0100

    Use upstream_branch to detect branch name in dlrn jobs
    rdopkg assumes stable/ prefix always for source_branch [1) while this is
    not longer correct for unmaintained releases.
    
    rdopkg has introduced new data upstream_branch [2] which properly get the
    information from upstream_branch metadata in rdoinfo. This patch is
    moving dlrn jobs in distgits to use that new upstream_branch data instead of
    source_branch.
    
    [1] https://github.com/softwarefactory-project/rdopkg/blob/e5721d98c878af38179c0c265a0706e3872dc0b9/rdopkg/actionmods/rdoinfo.py#L54
    [2] https://softwarefactory-project.io/r/c/rdopkg/+/30963
    
    Change-Id: I1a0a33c02bbf16b4617092e880874d0b8b57da98

--- playbooks/rdoinfo/files/rdoinfo-DLRN.sh
+++ playbooks/rdoinfo/files/rdoinfo-DLRN.sh
@@ -33,7 +33,7 @@ if [[ "${TAG}" != "caracal-uc" ]]; then
     branch=$(echo $TAG | awk -F- '{print $1}')
     tag=$TAG
     baseurl="http://trunk.rdoproject.org/${branch}/${CENTOS_VERSION}/"
-    src=$(rdopkg release -r ${TAG}  | grep source_branch | awk '{print $2}')
+    src=$(rdopkg release -r ${TAG}  | grep upstream_branch | awk '{print $2}')
     PROJECT_DISTRO_BRANCH="${TAG}-rdo"
 fi
 
